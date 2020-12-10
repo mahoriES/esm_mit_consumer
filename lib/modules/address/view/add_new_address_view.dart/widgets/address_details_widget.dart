73a1b6cbbaa3484a68bcce53e0ea7e14753c359d
Anushka <aagrawal6789@gmail.com>
2020-12-10 11:35:18 +0530

    replaced widget binding logic with onDidChange callback

--- lib/modules/address/view/add_new_address_view.dart/widgets/address_details_widget.dart
+++ lib/modules/address/view/add_new_address_view.dart/widgets/address_details_widget.dart
@@ -40,15 +40,14 @@ class _AddressDetailsWidgetState extends State<AddressDetailsWidget> {
         addressNameController.text =
             store.state.addressState.addressRequest.addressName;
       },
+      onDidChange: (snapshot) {
+        if (snapshot.isLoading) {
+          LoadingDialog.show();
+        } else {
+          LoadingDialog.hide();
+        }
+      },
       builder: (context, snapshot) {
-        WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-          if (snapshot.isLoading) {
-            LoadingDialog.show();
-          } else {
-            LoadingDialog.hide();
-          }
-        });
-
         return Container(
           padding: EdgeInsets.all(20.toWidth),
           color: CustomTheme.of(context).colors.backgroundColor,
